5063082d07ec3fe15c1d3167da8f8ec0a64be8b5
jorismilot <jorismilot@live.nl>
2025-07-25 16:16:38 +0200

    Fixed data loading, next calibration methods

--- calibration.py
+++ calibration.py
@@ -8,7 +8,9 @@ class Pricing:
         self.coins = ['btc', 'eth']
         self.dte = 0
 
+    # DATA LOADING AND STORAGE METHODS
     def load_0dte_data(self):
+        """        Load and process 0 DTE data for specified coins.         """
         for coin in self.coins:
             # Loading data 
             file_path = os.path.join(self.data_dir,'snapshot_data', f'{coin}_full_data_transformed.csv')
@@ -25,33 +27,71 @@ class Pricing:
             df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s', utc=True)
 
             # Extract from symbol:
-            df[['coin', 'raw_expiry', 'strike', 'raw_opt_code']] = df['symbol'].str.split('-', expand=True).iloc[:, :4]
+            df[['coin', 'expiry_raw', 'strike', 'raw_opt_code']] = df['symbol'].str.split('-', expand=True).iloc[:, :4]
             df['opt_type'] = df['raw_opt_code'].map({'C': 'call', 'P': 'put'}) # Map option type
 
             # Compute expiration and time to maturity
-            expiry_dt = pd.to_datetime(df['raw_expiry'], format='%d%b%y', errors='coerce')
-            df['expiration'] = (expiry_dt + pd.Timedelta(hours=8)).dt.tz_localize('UTC')
-            df['time_to_maturity'] = (df['expiration'] - df['timestamp']).dt.total_seconds()
-            df['DTE'] = np.floor(df['time_to_maturity'] / 86_400).astype('Int64')
+            df['expiry'] = (pd.to_datetime(df['expiry_raw'], format='%d%b%y', errors='coerce') + pd.Timedelta(hours=8)).dt.tz_localize('UTC')
+            df['time_to_maturity'] = (df['expiry'] - df['timestamp']).dt.total_seconds()
+            df = df[df['time_to_maturity'] > 0] # drop expired rows
+            df['DTE'] = (np.ceil(df['time_to_maturity'] / 86_400) - 1).astype('Int64')      
 
             # Drop intermediate columns
-            df = df.drop(columns=['raw_expiry', 'raw_opt_code'])
+            df = df.drop(columns=['expiry_raw', 'raw_opt_code'])
 
             # Reorder and filter 
-            df = df[cols_base + ['coin', 'expiration', 'time_to_maturity', 'DTE', 'strike', 'opt_type'] + cols_rest]
+            df = df[cols_base + ['coin', 'expiry', 'time_to_maturity', 'DTE', 'strike', 'opt_type'] + cols_rest]
             df = df[df['DTE'] == 0]
 
             # Save the filtered DataFrame
-            out_dir = os.path.join(self.data_dir, 'calibration_data')
+            out_dir = os.path.join(self.data_dir, 'calibration_data/all')
             os.makedirs(out_dir, exist_ok=True)
             df.to_csv(os.path.join(out_dir, f'{coin}_0dte_data.csv'), index=False)
+    
+    def store_time_specific_data(self, df, coin):
+        """  Store time-specific data for a given coin.  """
+        coin = coin.lower()
+
+        # parse timestamp and extract hour
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
+        df['hour']      = df['timestamp'].dt.hour
+
+        # Make hourly data sets
+        for hour in range(24):
+            # slice just this hour
+            df_hour = df[df['hour'] == hour].copy()
+            df_hour.drop(columns=['hour'], inplace=True)
+
+            # prepare output directory & filename
+            out_dir = os.path.join(self.data_dir, 'calibration_data', f'{hour:02d}')
+            os.makedirs(out_dir, exist_ok=True)
+
+            filename = f'{coin}_{hour:02d}_0dte_data.csv'
+            out_path = os.path.join(out_dir, filename)
+
+            # write CSV without index
+            df_hour.to_csv(out_path, index=False)
+
+    # CALIBRATION METHODS
+    def charact_func_merton(r,tau,muJ,sigmaJ,sigma, xi, S0):
+        i   = np.complex(0.0,1.0)
+        # Term for E(exp(J)-1)
+        helpExp = np.exp(muJ + 0.5 * sigmaJ * sigmaJ) - 1.0
+        
+        # Characteristic function for Merton's model    
+        cf = lambda u: np.exp(i*u*np.log(S0)) * np.exp(i * u * (r - xi * helpExp - 0.5 * sigma * sigma) *tau \
+            - 0.5 * sigma * sigma * u * u * tau + xi * tau * \
+            (np.exp(i * u * muJ - 0.5 * sigmaJ * sigmaJ * u * u)-1.0))
+        return cf 
+
+
+
+
 
 if __name__ == "__main__":
-    Pricing().load_0dte_data()
-    print("0 DTE data loaded and processed successfully.")
+    # Init
+    pricing = Pricing()
 
-    # Get df where timestamp equals expiration
-    for coin in ['btc', 'eth']:
-        df = pd.read_csv(os.path.join('/Users/joris/Documents/Master QF/Thesis/optimal-gamma-hedging/Data/calibration_data', f'{coin}_0dte_data.csv'))
-        df_exp = df[df['timestamp'] == df['expiration']]
-        print(f"Data for {coin} at expiration:\n", df_exp.head())
+
+
+
